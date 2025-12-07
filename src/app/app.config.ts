af83f5fd47103e3dbc5dac57f7965ee5991b7a68
Animesh5106 <animeshchakraborty2015@gmail.com>
2025-12-07 15:07:48 +0530

    add nginix file

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,5 +4,11 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes),
+      {
+      provide: 'nonce',
+      useValue: 'randomNonceGoesHere'
+    }
+
+  ]
 };
